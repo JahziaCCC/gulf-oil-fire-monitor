6e5546e6b56d32a4b140dcc75f1ad19094f342f7
JahziaCCC <s300s700@gmail.com>
2026-03-16 02:57:57 +0300

    Create reporter.py

---
+++ reporter.py
@@ -0,0 +1,14 @@
+import requests
+import os
+
+BOT = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT = os.environ["TELEGRAM_CHAT_ID"]
+
+def send_report(text):
+
+    url = f"https://api.telegram.org/bot{BOT}/sendMessage"
+
+    requests.post(url,json={
+        "chat_id":CHAT,
+        "text":text
+    })
