cb9304178a3b0d094e2dee521acf5fb0bac3ae15
JahziaCCC <s300s700@gmail.com>
2026-03-16 03:10:31 +0300

    Update air_quality.py

--- air_quality.py
+++ air_quality.py
@@ -1,11 +1,4 @@
-import random
-
 def get_air_quality():
 
-    cities = {
-        "الكويت": random.randint(70,150),
-        "الدمام": random.randint(60,130),
-        "المنامة": random.randint(40,100)
-    }
-
-    return cities
+    # لا توجد بيانات حالياً
+    return {}
